d53e2a6fd2167edb7e6ec08fd0eab99b39f206ea
Md. Minhazul Haque <1847242+mdminhazulhaque@users.noreply.github.com>
2018-10-16 18:22:51 +0600

    Update tabletojson.py

--- tabletojson.py
+++ tabletojson.py
@@ -1,52 +1,37 @@
 import json
 from bs4 import BeautifulSoup
 
-content = """<table>
-    <thead>
-        <th>ID</th>
-        <th>Vendor</th>
-        <th>Product</th>
-    </thead>
-    <tr>
-        <td>1</td>
-        <td>Intel</td>
-        <td>Processor</td>
-    </tr>
-    <tr>
-        <td>2</td>
-        <td>AMD</td>
-        <td>GPU</td>
-    </tr>
-    <tr>
-        <td>3</td>
-        <td>Gigabyte</td>
-        <td>Mainboard</td>
-    </tr>
-</table>"""
-
-#with open('in.xml', 'r') as infile:
-    #content = infile.read()
-
-soup = BeautifulSoup(content, "lxml")
-rows = soup.find_all("tr")
-
-headers = {}
-
-thead = soup.find("thead").find_all("th")
-
-for i in range(len(thead)):
-    headers[i] = thead[i].text.strip().lower()
-
-data = []
-
-for row in rows:
-    cells = row.find_all("td")
+def html_to_json(content, indent=None):
+    soup = BeautifulSoup(content, "lxml")
+    rows = soup.find_all("tr")
     
-    item = {}
-    
-    for index in headers:
-        item[headers[index]] = cells[index].text
-    
-    data.append(item)
+    headers = {}
+    thead = soup.find("thead")
+    if thead:
+        thead = thead.find_all("th")
+        for i in range(len(thead)):
+            headers[i] = thead[i].text.strip().lower()
+    data = []
+    for row in rows:
+        cells = row.find_all("td")
+        if thead:
+            items = {}
+            for index in headers:
+                items[headers[index]] = cells[index].text
+        else:
+            items = []
+            for index in cells:
+                items.append(index.text.strip())
+        data.append(items)
+    return json.dumps(data, indent=indent)
 
-print(json.dumps(data, indent=4))
+content = "<table> <thead> <th>ID</th> <th>Vendor</th> <th>Product</th> </thead> <tr> <td>1</td> <td>Intel</td> <td>Processor</td> </tr> <tr> <td>2</td> <td>AMD</td> <td>GPU</td> </tr> <tr> <td>3</td> <td>Gigabyte</td> <td>Mainboard</td> </tr></table>"
+content_no_thead = "<table> <tr> <td>1</td> <td>Intel</td> <td>Processor</td> </tr> <tr> <td>2</td> <td>AMD</td> <td>GPU</td> </tr> <tr> <td>3</td> <td>Gigabyte</td> <td>Mainboard</td> </tr></table>"
+
+print(html_to_json(content))
+# returns list of objects as header is present
+# [{"id": "1", "vendor": "Intel", "product": "Processor"}, {"id": "2", "vendor": "AMD", "product": "GPU"}, {"id": "3", "vendor": "Gigabyte", "product": "Mainboard"}]
+
+print(html_to_json(content_no_thead, indent=4))
+# autodetects if thead is there, returns list of list
+# [["1", "Intel", "Processor"], ["2", "AMD", "GPU"], ["3", "Gigabyte", "Mainboard"]]
